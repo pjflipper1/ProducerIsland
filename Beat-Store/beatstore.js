c0e36d7b98bf42749b3844235d3d62f7fd4ae043
Sujay Subramanyam <61365780+pjflipper1@users.noreply.github.com>
2023-11-24 00:03:03 -0800

    Build a brand new beat store page

--- Beat-Store/beatstore.js
+++ Beat-Store/beatstore.js
@@ -1,26 +1,60 @@
-// Template of all the data that each beat c
 let ProducerNames = ["CaptainSuj", "LinXX"];
 
 class Beat {
-    constructor() {
-        this.BeatTitle = ["From Me To You", "In The Room"];
-        this.Producer = [0, 0];
-        this.BeatFile = [
-            'pjflipper1/ProducerIsland/Beat MP3 Files /(FREE FOR PROFIT) Quadeca From Me To You Type Beat.mp3',
-            'pjflipper1/ProducerIsland/Beat MP3 Files /hardest in the room.mp3'
-        ];
+    constructor(title, BPM, Tags, producerIndex, Key, beatFile) {
+        this.title = title;
+        this.Bpm = this.Bpm;
+        this.Tags = Tags;
+        this.producer = ProducerNames[producerIndex];
+        this.Key = Key;
+        this.beatFile = beatFile;
     }
 }
 
-// Functions that will be called at the start of the program
-function DisplayBeat() {
-    // Create an instance of the Beat class
-    const myBeat = new Beat();
+    // Example data (replace this with your actual data)
+    const beatsData = [
+        new Beat("From Me To You", 128, ["Quadeca", "From Me To You", "Dreamy"], 0, "Am", 'pjflipper1/ProducerIsland/Beat MP3 Files /(FREE FOR PROFIT) Quadeca From Me To You Type Beat.mp3'),
+        new Beat("In The Room", 128, ["Travis Scott", "JACKBOYS", "Highest In The Room"], 1, "Am", 'pjflipper1/ProducerIsland/Beat MP3 Files /hardest in the room.mp3')
+    ];
 
-    // Access the BeatTitle property using the instance
-    var Title = document.getElementById("hello");
-    Title.textContent = myBeat.BeatTitle[0];
-}
+    // Function to generate table rows based on data
+    function generateTableRows(data) {
+        const tableBody = document.getElementById('tableBody');
 
-// Call the DisplayBeat function
-DisplayBeat();
+        data.forEach(beat => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td><h2>${beat.title}</h2></td>
+                <td><h2>${beat.Bpm}</h2></td>
+                <td><h2>${beat.Tags}</h2></td>
+                <td><h2>${beat.producer}</h2></td>
+                <td><h2>${beat.Key}</h2></td>
+                <td><button onclick="downloadBeat('${beat.beatFile}')">Download</button></td>
+            `;
+            tableBody.appendChild(row);
+        });
+    }
+
+    // Call the function with your data to generate table rows
+    generateTableRows(beatsData);
+
+    // Function to handle beat download
+    function downloadBeat(FileLocation) {
+        // Create an invisible anchor element
+        var anchor = document.createElement('a');
+        anchor.style.display = 'none';
+    
+        // Set the href and download attributes to trigger the download
+        anchor.href = FileLocation;
+        anchor.download = 'downloaded_file.mp3';
+    
+        // Append the anchor to the body
+        document.body.appendChild(anchor);
+    
+        // Trigger a click on the anchor
+        anchor.click();
+    
+        // Remove the anchor from the body
+        document.body.removeChild(anchor);
+      }
+    
